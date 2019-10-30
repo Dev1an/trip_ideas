f8103858b2b26e2d4ac399d54daf9a64c0cca995
Wouter Depamelaere <wouter.depamelaere@hotmail.com>
2019-10-30 22:55:33 +0100

    Detail - Added database, lookup destination details
    A singleton database with helper classes, and data models for Destinations and Favorites&Visited is added.

---
+++ lib/database_helpers.dart
@@ -0,0 +1,218 @@
+import 'dart:io';
+import 'package:path/path.dart' as p;
+import 'package:sqflite/sqflite.dart';
+
+// Destinations database table and column names
+final String tableDestinations = 'destinations';
+final String columnId = '_id';
+final String columnDestinationCity = 'destination';
+final String columnDestinationCountry = 'country';
+final String columnLocation = 'location';
+final String columnDescription = 'description';
+
+final String tableFavorites = 'favorites';
+
+final String tableVisited = 'visited';
+
+// data model class
+class Destination {
+
+  int id;
+  String destination;
+  String country;
+  String location;
+  String description;
+
+  Destination();
+
+  // convenience constructor to create a Destination object from a Map
+  Destination.fromMap(Map<String, dynamic> map) {
+    id = map[columnId];
+    destination = map[columnDestinationCity];
+    country = map[columnDestinationCountry];
+    location = map[columnLocation];
+    description = map[columnDescription];
+  }
+
+  // convenience method to create a Map from this Destination object
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic>{
+      columnDestinationCity: destination,
+      columnDestinationCountry: country,
+      columnLocation: location,
+      columnDescription: description
+    };
+    if (id != null) {
+      map[columnId] = id;
+    }
+    return map;
+  }
+}
+
+class FavoriteOrVisited {
+
+  int id;
+  String destination;
+
+  FavoriteOrVisited();
+
+  // convenience constructor to create a Favorite object from a Map
+  FavoriteOrVisited.fromMap(Map<String, dynamic> map) {
+    id = map[columnId];
+    destination = map[columnDestinationCity];
+  }
+
+  // convenience method to create a Map from this Favorite object
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic>{
+      columnDestinationCity: destination
+    };
+    if (id != null) {
+      map[columnId] = id;
+    }
+    return map;
+  }
+}
+
+
+
+// singleton class to manage the database
+class DatabaseHelper {
+
+  // This is the actual database filename that is saved in the docs directory.
+  static final _databaseName = "TripIdeasDatabase.db";
+  // Increment this version when you need to change the schema.
+  static final _databaseVersion = 1;
+
+  // Make this a singleton class.
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  // Only allow a single open connection to the database.
+  static Database _database;
+  Future<Database> get database async {
+    if (_database != null) return _database;
+    _database = await _initDatabase();
+    return _database;
+  }
+
+  // open the database
+  _initDatabase() async {
+    // The path_provider plugin gets the right directory for Android or iOS.
+    //Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    //String path = join(documentsDirectory.path, _databaseName);
+
+    var databasesPath = await getDatabasesPath();
+    String path = p.join(databasesPath,_databaseName);
+
+// Make sure the directory exists
+    try {
+
+      await Directory(databasesPath).create(recursive: true);
+    } catch (e) {print(e);}
+
+    // Open the database. Can also add an onUpdate callback parameter.
+    return await openDatabase(path,
+        version: _databaseVersion,
+        onCreate: _onCreate);
+  }
+
+  // SQL string to create the database
+  Future _onCreate(Database db, int version) async {
+
+    await db.execute('''
+              CREATE TABLE $tableDestinations (
+                $columnId INTEGER PRIMARY KEY AUTOINCREMENT,
+                $columnDestinationCity TEXT NOT NULL,
+                $columnDestinationCountry TEXT NOT NULL,
+                $columnLocation TEXT NOT NULL,
+                $columnDescription TEXT NOT NULL
+              )
+              ''');
+    /*await db.execute('''
+       CREATE TABLE $tableFavorites (
+        $columnId INTEGER PRIMARY KEY,
+        $columnDestinationCity TEXT NOT NULL
+       )''');
+    await db.execute('''
+       CREATE TABLE $tableVisited (
+        $columnId INTEGER PRIMARY KEY,
+        $columnDestinationCity TEXT NOT NULL
+       )''');*/
+  }
+
+  // Database helper methods:
+  // -------- DESTINATIONS --------
+  Future<int> insertDestination(Destination dest) async {
+    Database db = await database;
+    int id = await db.insert(tableDestinations, dest.toMap());
+    return id;
+  }
+
+  Future<Destination> queryDestination(int id) async {
+    Database db = await database;
+    List<Map> maps = await db.query(tableDestinations,
+        columns: [columnId, columnDestinationCity, columnDestinationCountry,columnLocation,columnDescription],
+        where: '$columnId = ?',
+        whereArgs: [id]);
+    if (maps.length > 0) {
+      return Destination.fromMap(maps.first);
+    }
+    return null;
+  }
+
+// TO DO: queryAllDestinations()
+// TO DO: delete(int id)
+// TO DO: update(Destination destination)
+
+  // -------- FAVORITE OR VISITED --------
+  // INSERT
+  Future<int> insertFavorite(FavoriteOrVisited favOrVis) async {
+    return insertFavoriteOrVisited(favOrVis, tableFavorites);
+  }
+
+  Future<int> insertVisited(FavoriteOrVisited favOrVis) async {
+    return insertFavoriteOrVisited(favOrVis, tableVisited);
+  }
+
+  Future<int> insertFavoriteOrVisited(FavoriteOrVisited favOrVis, String table) async {
+    Database db = await database;
+    int id = await db.insert(table, favOrVis.toMap());
+    return id;
+  }
+
+  // DELETE
+  Future<int> deleteFavorite(int id) async {
+    return deleteFavoriteOrVisited(id, tableFavorites);
+  }
+
+  Future<int> deleteVisited(int id) async {
+    return deleteFavoriteOrVisited(id, tableVisited);
+  }
+
+  Future<int> deleteFavoriteOrVisited(int id,String table) async {
+    Database db = await database;
+    return await db.delete(table, where: '$columnId = ?', whereArgs: [id]);
+  }
+
+  // QUERY ALL
+  Future<List<FavoriteOrVisited>> queryAllFavorites() async {
+    return queryAllFavoriteOrVisited(tableFavorites);
+  }
+  Future<List<FavoriteOrVisited>> queryAllVisited() async {
+    return queryAllFavoriteOrVisited(tableVisited);
+  }
+
+  Future<List<FavoriteOrVisited>> queryAllFavoriteOrVisited(String table) async {
+    Database db = await database;
+    List<Map> maps = await db.query(table); //SELECT *
+    if (maps.length > 0) {
+      return maps.map((favOrVis) => FavoriteOrVisited.fromMap(favOrVis)).toList();
+    }
+    return null;
+  }
+
+
+
+}
+
--- lib/detail.dart
+++ lib/detail.dart
@@ -3,6 +3,7 @@ import 'custom_icons.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'database_helpers.dart';
 
 class DetailWidget extends StatefulWidget {
   @override
@@ -11,6 +12,16 @@ class DetailWidget extends StatefulWidget {
 
 class _DetailWidgetState extends State<DetailWidget> {
   static const int PHOTOS_AMOUNT = 5;
+  int currentDestID = 1;
+  Destination currentDestination = new Destination();
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeDestinationDB();
+    _loadDetailsOfCurrent();
+  }
+
   // Handling favorite toggle
   bool _favorite = false;
   void _handleFavoriteChanged(bool newValue) {
@@ -29,32 +40,33 @@ class _DetailWidgetState extends State<DetailWidget> {
 
   Future<List<String>> photoUrls;
   int calledBuild = 0;
+
   // The DETAIL build method
   @override
   Widget build(BuildContext context) {
     calledBuild++;
-    if(calledBuild==1) photoUrls = getImageUrls("paris");
+    if (calledBuild == 2 && currentDestination.destination!="") photoUrls = getImageUrls(currentDestination.destination);
 
     // Image carousel
     Widget photoSection = new Container(
-      child:  new Swiper(
-        itemBuilder: (BuildContext context,int index){
+      child: new Swiper(
+        itemBuilder: (BuildContext context, int index) {
           return FutureBuilder(
             future: photoUrls,
-            builder: (context,snapshot) {
-              if(snapshot.connectionState == ConnectionState.done) {
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
                 return Image.network(snapshot.data[index]);
               } else {
-                return new SizedBox(
-                    width: 20.0,
-                    height: 20.0,
-                    child: CircularProgressIndicator()
-                );
+                return new Container(
+                    child: new Center(
+                      child: new SizedBox(
+                          width: 20.0,
+                          height: 20.0,
+                          child: CircularProgressIndicator()),
+                ));
               }
             },
           );
-           // new Image.network(urls[index],fit: BoxFit.fill);
-          //return new Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill,);
         },
         itemCount: PHOTOS_AMOUNT,
         viewportFraction: 0.8,
@@ -63,8 +75,7 @@ class _DetailWidgetState extends State<DetailWidget> {
         control: new SwiperControl(),
       ),
       height: 200,
-    ) ;
-
+    );
 
     // Title row
     Widget titleSection = Container(
@@ -80,14 +91,14 @@ class _DetailWidgetState extends State<DetailWidget> {
                 Container(
                   padding: const EdgeInsets.only(bottom: 8),
                   child: Text(
-                    'Paris',
+                    currentDestination.destination,
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
                 Text(
-                  'France, Europe',
+                  currentDestination.country,
                   style: TextStyle(
                     color: Colors.grey[500],
                   ),
@@ -112,8 +123,9 @@ class _DetailWidgetState extends State<DetailWidget> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-          FavoriteWidget(favorite: _favorite,onChanged: _handleFavoriteChanged),
-          VisitedWidget(visited: _visited,onChanged: _handleVisitedChanged),
+          FavoriteWidget(
+              favorite: _favorite, onChanged: _handleFavoriteChanged),
+          VisitedWidget(visited: _visited, onChanged: _handleVisitedChanged),
         ],
       ),
     );
@@ -122,10 +134,7 @@ class _DetailWidgetState extends State<DetailWidget> {
     Widget textSection = Container(
       padding: const EdgeInsets.all(32),
       child: Text(
-        'Paris offers the largest concentration of tourist attractions in France, and possibly in Europe. '
-        'Besides some of the world\'s most famous musuems, its has a vibrant historic city centre, a beautiful '
-        'riverscape, an extensive range of historic monuments, including cathedrals, chapels and palaces, plus one of the most famous nightlife scenes in the world.'
-        'Paris is also famous for its cafés and restaurants, its theatres and cinemas, and its general ambiance.',
+        currentDestination.description,
         softWrap: true,
       ),
     );
@@ -164,6 +173,8 @@ class _DetailWidgetState extends State<DetailWidget> {
         ));
   }
 
+  //=============== HELPER METHODS ===================
+
   // Helper method to create columns
   Column _buildButtonColumn(Color color, IconData icon, String label) {
     return Column(
@@ -194,32 +205,108 @@ class _DetailWidgetState extends State<DetailWidget> {
     );
   }
 
-
   // Helper method to retrieve images
-  Future<List<String>> getImageUrls(String city) async{
+  Future<List<String>> getImageUrls(String city) async {
     String exlude = "+-woman+-animal+-flower+-meal+-postcard+-door+-painting";
     int resultLimit = 40;
-    String url = "https://pixabay.com/api/?key=14114941-200003d620e7a15f560cb840c";
-    url += "&q="+city+exlude+"&image_type=photo&orientation=horizontal&category=travel&page=1&per_page="+resultLimit.toString();
+    String url =
+        "https://pixabay.com/api/?key=14114941-200003d620e7a15f560cb840c";
+    url += "&q=" +
+        city +
+        exlude +
+        "&image_type=photo&orientation=horizontal&category=travel&page=1&per_page=" +
+        resultLimit.toString();
 
-    List<String> list =  new List();
+    List<String> list = new List();
 
     final response = await http.get(url);
     var data = json.decode(response.body);
     List<dynamic> hits = data['hits'];
 
-    var randomIndices = new List<int>.generate(resultLimit, (int index) => index); // [0, 1, 4]
+    var randomIndices =
+        new List<int>.generate(resultLimit, (int index) => index); // [0, 1, 4]
     randomIndices.shuffle();
-    print("HITS LENGHTH");
-    print(hits.length);
 
-    for(int i=0; i<PHOTOS_AMOUNT;i++) {
+    for (int i = 0; i < PHOTOS_AMOUNT; i++) {
       //list.add(hits[i]["webformatURL"]);
       list.add(hits[randomIndices[i]]["webformatURL"]);
     }
     return list;
   }
 
+  // Helper method to read from database
+  Future<Destination> _readDestination(int destID) async {
+    DatabaseHelper helper = DatabaseHelper.instance;
+    Destination dest = await helper.queryDestination(destID);
+    if (dest == null) {
+      print('read row $destID: empty');
+      return null;
+    } else {
+      print('read row $destID: ${dest.destination} ${dest.country}');
+      return dest;
+    }
+  }
+
+  _addFavorite(FavoriteOrVisited fv) async {
+    DatabaseHelper helper = DatabaseHelper.instance;
+    int id = await helper.insertFavorite(fv);
+    print('inserted row: $id');
+  }
+
+  _addVisited(FavoriteOrVisited fv) async {
+    DatabaseHelper helper = DatabaseHelper.instance;
+    int id = await helper.insertVisited(fv);
+    print('inserted row: $id');
+  }
+
+  _deleteFavorite(FavoriteOrVisited fv) async {
+    DatabaseHelper helper = DatabaseHelper.instance;
+    await helper.deleteFavorite(fv.id);
+  }
+
+  _deleteVisited(FavoriteOrVisited fv) async {
+    DatabaseHelper helper = DatabaseHelper.instance;
+    await helper.deleteVisited(fv.id);
+  }
+
+  _initializeDestinationDB() async {
+    DatabaseHelper helper = DatabaseHelper.instance;
+    Destination dest = new Destination();
+    dest.destination = "Paris";
+    dest.country = "France";
+    dest.location = "48.8566° N, 2.3522° E";
+    dest.description = "Paris offers the largest concentration of tourist attractions in France, and possibly in Europe."+
+                      "Besides some of the world\'s most famous musuems, its has a vibrant historic city centre, a beautiful "+
+                      "riverscape, an extensive range of historic monuments, including cathedrals, chapels and palaces, plus "+
+                      "one of the most famous nightlife scenes in the world. Paris is also famous for its cafés and "+
+                      "restaurants, its theatres and cinemas, and its general ambiance.";
+    await helper.insertDestination(dest);
+    dest = new Destination();
+    dest.destination = "Barcelona";
+    dest.location = "41.3851° N, 2.1734° E";
+    dest.country = "Spain";
+    dest.description = "Barcelona is a city of contrasts: it's Catalan and Spanish, traditional "+
+                        "and modern, and exciting and laid-back, all at the same time. But it's "+
+                        "this perfect harmony that makes Spain's second-largest city fascinating "+
+                        "enough to draw around 32 million tourists every year. As one of Europe's "+
+                        "chicest cities, home to no shortage of things to see and do, it's important "+
+                        "to make every second count while in Barcelona.";
+    await helper.insertDestination(dest);
+  }
+
+  _loadDetailsOfCurrent() {
+    // set dummy values of placeholder destination
+    currentDestination.destination="";
+    currentDestination.description="";
+    currentDestination.location="";
+    currentDestination.country="";
+
+    _readDestination(currentDestID).then((destination) =>
+      setState(() {
+        currentDestination = destination;}
+      )
+    );
+  }
 }
 
 class FavoriteWidget extends StatelessWidget {
@@ -244,9 +331,8 @@ class FavoriteWidget extends StatelessWidget {
           margin: const EdgeInsets.only(top: 0),
           padding: EdgeInsets.all(0),
           child: IconButton(
-            icon: (favorite
-                ? Icon(Icons.favorite)
-                : Icon(Icons.favorite_border)),
+            icon:
+                (favorite ? Icon(Icons.favorite) : Icon(Icons.favorite_border)),
             color: color,
             onPressed: _handleTap,
           ),
@@ -254,7 +340,7 @@ class FavoriteWidget extends StatelessWidget {
         Container(
           margin: const EdgeInsets.only(top: 8),
           child: Text(
-            favorite ? 'FAVORITE' : 'NOT FAVORITE',
+            'FAVORITE',
             style: TextStyle(
               fontSize: 12,
               fontWeight: FontWeight.w400,
@@ -265,7 +351,6 @@ class FavoriteWidget extends StatelessWidget {
       ],
     );
   }
-
 }
 
 class VisitedWidget extends StatelessWidget {
@@ -300,7 +385,7 @@ class VisitedWidget extends StatelessWidget {
         Container(
           margin: const EdgeInsets.only(top: 8),
           child: Text(
-            visited ? 'VISITED' : 'NOT VISITED',
+            'VISITED',
             style: TextStyle(
               fontSize: 12,
               fontWeight: FontWeight.w400,
@@ -311,6 +396,5 @@ class VisitedWidget extends StatelessWidget {
       ],
     );
   }
-
 }
 
